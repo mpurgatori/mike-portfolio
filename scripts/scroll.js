bf95b574387e67d049d795b6ed447062665b1226
Mike Purgatori <mpurgatori@gmail.com>
2018-08-05 11:20:59 -0500

    scroll file

---
+++ scripts/scroll.js
@@ -0,0 +1,4 @@
+const introArrow = document.getElementById("intro-arrow");
+const aboutArrow = document.getElementById("about-arrow");
+const portfolioArrow = document.getElementById("portfolio-arrow");
+
