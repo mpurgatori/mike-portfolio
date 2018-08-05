405509be8ebf2fc671dadfeb7b050bd34eff0f67
Mike Purgatori <mpurgatori@gmail.com>
2018-08-04 21:50:27 -0500

    portfolio styles

--- scripts/random-style.js
+++ scripts/random-style.js
@@ -3,6 +3,9 @@
   const colorCombos = [['#43A047', '#FAFAFA'], ['#485564', '#FAFAFA'], ['#303F9F', '#FAFAFA'], ['#AF3D4E', '#FAFAFA'], ['#FAFAFA', '#0277BD'], ['#FFEB3B', '#1E1E1E']];
   const mainElement = document.getElementsByTagName("main")[0];
   const linkElememts = document.getElementsByClassName("fab");
+
+  const portfolioItems = document.getElementsByClassName("portfolio-item");
+
   const colorPaletteIndex = Math.floor(Math.random() * colorCombos.length);
   const colorPalette = colorCombos[colorPaletteIndex];
   const backgroundColorIndex = Math.floor(Math.random() * colorPalette.length);
@@ -14,11 +17,24 @@
       color: fontColor
   };
 
+  const portfolioStyles = {
+    background: fontColor,
+    color: backgroundColor
+};
+
   const elementStyles = {
       color: fontColor
   };
 
-  Object.assign(mainElement.style, mainStyles);
-  Object.keys(linkElememts).forEach(function (key) {
-      Object.assign(linkElememts[key].style, elementStyles)
+
+const changeColor = () => {
+    Object.assign(mainElement.style, mainStyles);
+    Object.keys(linkElememts).forEach(function (key) {
+        Object.assign(linkElememts[key].style, elementStyles)
+    }); 
+    Object.keys(portfolioItems).forEach(function (key) {
+      Object.assign(portfolioItems[key].style, portfolioStyles)
   }); 
+}
+
+changeColor();
