3d3a49540e4de9be0c6f36b2348ac163cee98b0d
Mike Purgatori <mpurgatori@gmail.com>
2018-08-05 11:06:19 -0500

    colors complete, navbar, no consecutive color

--- scripts/random-style.js
+++ scripts/random-style.js
@@ -1,41 +1,55 @@
 
 
-  const colorCombos = [['#43A047', '#FAFAFA'], ['#485564', '#FAFAFA'], ['#303F9F', '#FAFAFA'], ['#AF3D4E', '#FAFAFA'], ['#FAFAFA', '#0277BD'], ['#FFEB3B', '#1E1E1E']];
-  const mainElement = document.getElementsByTagName("main")[0];
-  const linkElememts = document.getElementsByClassName("fab");
 
-  const portfolioItems = document.getElementsByClassName("portfolio-item");
-  const magicWand = document.getElementById("wand");
 
-  const colorPaletteIndex = Math.floor(Math.random() * colorCombos.length);
-  const colorPalette = colorCombos[colorPaletteIndex];
-  const backgroundColorIndex = Math.floor(Math.random() * colorPalette.length);
-  const backgroundColor = colorPalette[backgroundColorIndex];
-  const fontColor = colorPalette[colorPalette.length - backgroundColorIndex - 1];
+const magicWand = document.getElementById("wand");
+const mainElement = document.getElementsByTagName("main")[0];
+const navbar = document.getElementById("navbar");
+const linkElememts = document.getElementsByClassName("fab");
+const portfolioItems = document.getElementsByClassName("portfolio-item");
 
-  const mainStyles = {
-      background: backgroundColor,
-      color: fontColor
-  };
-
-  const portfolioStyles = {
-    background: fontColor,
-    color: backgroundColor
-};
-
-  const elementStyles = {
-      color: fontColor
-  };
+let currentColor = '';
 
 
 const changeColor = () => {
-    Object.assign(mainElement.style, mainStyles);
-    Object.keys(linkElememts).forEach(function (key) {
-        Object.assign(linkElememts[key].style, elementStyles)
-    }); 
-    Object.keys(portfolioItems).forEach(function (key) {
-      Object.assign(portfolioItems[key].style, portfolioStyles)
-  }); 
+
+    const colorCombos = [['#43A047', '#FAFAFA'], ['#485564', '#FAFAFA'], ['#303F9F', '#FAFAFA'], ['#AF3D4E', '#FAFAFA'], ['#FAFAFA', '#0277BD'], ['#FFEB3B', '#1E1E1E']];
+    const colorPaletteIndex = Math.floor(Math.random() * colorCombos.length);
+    const colorPalette = colorCombos[colorPaletteIndex];
+    if (currentColor.toString() === colorPalette.toString())
+    {
+        changeColor();
+    }
+    else 
+    {
+        currentColor = colorPalette;
+        const backgroundColorIndex = Math.floor(Math.random() * colorPalette.length);
+        const backgroundColor = colorPalette[backgroundColorIndex];
+        const fontColor = colorPalette[colorPalette.length - backgroundColorIndex - 1];
+      
+        const mainStyles = {
+            background: backgroundColor,
+            color: fontColor
+        };
+      
+        const portfolioStyles = {
+          background: fontColor,
+          color: backgroundColor
+      };
+      
+        const elementStyles = {
+            color: fontColor
+        };
+    
+        Object.assign(mainElement.style, mainStyles);
+        Object.assign(navbar.style, mainStyles);
+        Object.keys(linkElememts).forEach(function (key) {
+            Object.assign(linkElememts[key].style, elementStyles)
+        }); 
+        Object.keys(portfolioItems).forEach(function (key) {
+          Object.assign(portfolioItems[key].style, portfolioStyles)
+      }); 
+    }
 }
 
 
@@ -44,4 +58,13 @@ magicWand.addEventListener('click', ()=> {
 });
 
 
-changeColor();
+
+(function(count) {
+    if (count < 5) {
+        changeColor();
+        const caller = arguments.callee; 
+        setTimeout(function() {
+            caller(count + 1);
+        }, 500);    
+    }
+})(0);
